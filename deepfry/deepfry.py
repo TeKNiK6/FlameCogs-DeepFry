444f69fad0243b38752c06be842cc48494d165c3
TeKNiK6 <52848733+TeKNiK6@users.noreply.github.com>
2025-08-19 21:56:16 -0400

    fixed indents

--- deepfry/deepfry.py
+++ deepfry/deepfry.py
@@ -39,7 +39,7 @@ class Deepfry(commands.Cog):
 				f'Unsupported filetype'
 			)
 		try:
-    		ffmpeg.input(imgLink).output(outputFile, vf='scale=360:-1:flags=lanczos,eq=saturation=2.547872526587171:contrast=1.663945577845754:gamma_r=1.6468562525223558:gamma_g=1.982674184755574:gamma_b=1.884297295698892,noise=alls=15.554466360495802:allf=t,unsharp=5:5:3.25:5:5:3,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse').run()
+			ffmpeg.input(imgLink).output(outputFile, vf='scale=360:-1:flags=lanczos,eq=saturation=2.547872526587171:contrast=1.663945577845754:gamma_r=1.6468562525223558:gamma_g=1.982674184755574:gamma_b=1.884297295698892,noise=alls=15.554466360495802:allf=t,unsharp=5:5:3.25:5:5:3,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse').run()
 		except ffmpeg.Error as e:
     		raise ImageFindError(
 					f'Error frying image.'
