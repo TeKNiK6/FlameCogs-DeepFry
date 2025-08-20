e6976e81a8ee54e559400a0196e408000dbc0c45
TeKNiK6 <52848733+TeKNiK6@users.noreply.github.com>
2025-08-20 00:08:57 -0400

    Added tmpPath

--- deepfry/deepfry.py
+++ deepfry/deepfry.py
@@ -42,8 +42,14 @@ class Deepfry(commands.Cog):
 			raise ImageFindError(
 				f'Unsupported filetype'
 			)
-		tempDir = tempfile.TemporaryDirectory()
-		outputFileFullPath = tempDir.name + "/" + outputFile
+		tmpPath = "/tmp/temp_deepfry/"
+		if not os.path.exists(tmpPath):
+			try:
+				os.makedirs(tmpPath)
+			except OSError as e:
+				raise ImageFindError(f"Error frying image - {e}")
+
+		outputFileFullPath = tmpPath + "/" + outputFile
 
 		try:
 			ffmpeg.input(imgLink).output(outputFileFullPath, vf='scale=360:-1:flags=lanczos,eq=saturation=2.547872526587171:contrast=1.663945577845754:gamma_r=1.6468562525223558:gamma_g=1.982674184755574:gamma_b=1.884297295698892,noise=alls=15.554466360495802:allf=t,unsharp=5:5:3.25:5:5:3,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse').run(capture_stderr=True)
